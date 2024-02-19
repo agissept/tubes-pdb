271dd8058d332926cd6a5ceebab8114e09dcdd59
difawitsqard <difawitsqard@gmail.com>
2024-02-20 06:26:43 +0700

    add crud transaksi

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -1,17 +1,20 @@
 import path from "path";
 import express from "express";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.sendFile(path.resolve('./public/page/index.html'));
-}).get('/users', async (req, res) => {
-    res.sendFile(path.resolve('./public/page/users.html'));
-}).get('/products', async (req, res) => {
-    res.sendFile(path.resolve('./public/page/products.html'));
-})
+router
+  .get("/", async (req, res) => {
+    res.sendFile(path.resolve("./public/page/index.html"));
+  })
+  .get("/users", async (req, res) => {
+    res.sendFile(path.resolve("./public/page/users.html"));
+  })
+  .get("/products", async (req, res) => {
+    res.sendFile(path.resolve("./public/page/products.html"));
+  })
+  .get("/transactions", async (req, res) => {
+    res.sendFile(path.resolve("./public/page/transactions.html"));
+  });
 
-export default router
-
-
-
+export default router;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import pages from "./routes/pages.js";
 import apiUsers from "./routes/api-users.js";
 import apiProducts from "./routes/api-products.js";
+import apiTransactions from "./routes/api-transactions.js";
 
 const app = express();
 const port = 3000;
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", pages);
 app.use("/api", apiUsers);
 app.use("/api", apiProducts);
+app.use("/api", apiTransactions);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
